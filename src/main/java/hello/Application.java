533befbc09a3b3f994dbec8afb855ac8cc809217
Dhanwanthari Ramakrishnan <ramakrishnan.d@flipkart.com>
2017-01-19 18:29:28 +0530

    initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ramakrishnan.d on 19/01/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String [] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
